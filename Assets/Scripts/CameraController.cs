f4f1ba8ec3f5431c519c70ac5f098cf5f98b38ee
David-PC\David <david.borgondo@orange.fr>
2019-05-20 19:12:08 +0200

    V_0.1

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private BoxCollider MovingPoint;
+    private bool HasCollided;
+
+    void Start()
+    {
+        HasCollided = false;
+        MovingPoint = GetComponent<BoxCollider>();
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+            if ((other.GetComponent<Rigidbody2D>().velocity.x>0)&(other.gameObject.CompareTag("Player"))&(HasCollided==false))
+            {
+                transform.position = new Vector3(transform.position.x + (other.GetComponent<Rigidbody2D>().velocity.x) / 50, transform.position.y, transform.position.z);
+                HasCollided = true;
+            }
+    }
+
+    void Update()
+    {
+        HasCollided = false;
+    }
+}
---
+++ Assets/Scripts/PlayerControler.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControler : MonoBehaviour
+{
+    public float Speed;
+    public float RotateSpeed;
+    public KeyCode Up;
+    public KeyCode Down;
+    public object OtherPlayer;
+    private Rigidbody2D rb2d;
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        rb2d.AddForce(transform.right*Speed);
+        if (Input.GetKey(Down))
+        {
+            transform.Rotate(new Vector3(0, 0, -RotateSpeed));
+        }
+        else if (Input.GetKey(Up))
+        {
+            transform.Rotate(new Vector3(0, 0, RotateSpeed));
+        }
+    }
+}
